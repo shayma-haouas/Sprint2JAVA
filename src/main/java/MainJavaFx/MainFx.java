8919a92ca58ba6766a5cf60537f2e447256f91a0
Siwarachour <siwar.achour@esprit.tn>
2024-04-20 16:43:00 +0100

    Ajout env JavaFX

---
+++ src/main/java/MainJavaFx/MainFx.java
@@ -0,0 +1,34 @@
+package MainJavaFx;
+
+
+
+    import javafx.application.Application;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Parent;
+    import javafx.scene.Scene;
+    import javafx.scene.input.MouseEvent;
+    import javafx.scene.paint.Color;
+    import javafx.stage.Stage;
+    import javafx.stage.StageStyle;
+
+public class MainFx extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/UserInterface/Registration.fxml"));
+            Scene scene = new Scene(root);
+
+
+            stage.setScene(scene);
+
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/utils/MyDatabase.java
+++ src/main/java/utils/MyDatabase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class MyDatabase {
 
-    final String URL = "jdbc:mysql://localhost:3306/flo_dbtfin";
+    final String URL = "jdbc:mysql://localhost:3306/flo_dbt";
 
     final String USERNAME = "root";
     final String PASSWORD = "";
